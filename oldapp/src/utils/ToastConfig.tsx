959d97b1d527b1ffd80973e4883d0b5a69cea77e
Rutvik Makwana <rutvik.bspl@gmail.com>
2026-03-09 12:19:17 +0530

    nn

--- oldapp/src/utils/ToastConfig.tsx
+++
@@ -1,9 +0,0 @@
-// Using default toast message design
-// This file can be removed if you want to use completely default toast
-// Just remove the config prop from Toast component in App.tsx
-
-const toastConfig = {
-  // Empty config - uses default toast design
-};
-
-export default toastConfig;
